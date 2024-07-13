b16d06b59dfee147716e016985319102da34669d
nguyen tuan <tuanhb2k4@gmail.com>
2024-07-13 15:26:30 +0700

    ok

--- spring-boot/src/main/java/com/javaweb/api/BuildingAPI.java
+++ spring-boot/src/main/java/com/javaweb/api/BuildingAPI.java
@@ -1,47 +1,21 @@
 package com.javaweb.api;
 
-import java.awt.List;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.javaweb.customexception.FieldRequiredException;
 import com.javaweb.model.BuildingDTO;
-import com.javaweb.model.ErrotResponseDTO;
 import com.javaweb.service.BuildingService;
-import com.mysql.cj.jdbc.Driver;
-import com.mysql.cj.xdevapi.Statement;
+
+
 
 @RestController
-
 public class BuildingAPI {
-	 @Autowired
-	 private BuildingService buildingService;
-	@GetMapping(value="/api/building")
-	public ArrayList<BuildingDTO> getBuilding(@RequestParam(value="name") String name,
-												@RequestParam(value="district1") String district) {
-		ArrayList<BuildingDTO> result =buildingService.findAll(name);
-		return result;
-	}
-	
-	
+	@Autowired
 	@DeleteMapping(value="/api/building/{id}/{name}")
 	public void deleteBuilding(@PathVariable Integer id,
 							  @PathVariable String name) {
